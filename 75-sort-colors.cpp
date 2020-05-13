d6fc30589e5aa169157fc326699cfe85996793a6
Suraj Kumbhar <surajkannankumbhar@gmail.com>
2020-05-13 16:43:00 +0530

    Added an optimized solution using dutch nation flag algo for leetcode problem 75 sort colors

--- 75-sort-colors.cpp
+++ 75-sort-colors.cpp
@@ -24,26 +24,58 @@
 // First, iterate the array counting number of 0's, 1's, and 2's, then overwrite array with total number of 0's, then 1's and followed by 2's.
 // Could you come up with a one-pass algorithm using only constant space?
 
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+//         int r = 0;
+//         int w = 0;
+//         int b = 0;
+//         for (auto i : nums) {
+//             if (i == 0) r++;
+//             else if (i == 1) w++;
+//             else b++;
+//         }
+//         int i = 0;
+//         for (i; i < r; i++) {
+//             nums[i] = 0;
+//         }
+//         for (i = r; i < r + w; i++) {
+//             nums[i] = 1;
+//         }
+//         for (i = r + w; i < r + w + b; i++) {
+//             nums[i] = 2;
+//         }
+//     }
+// };
+
+// Dutch Nation Flag Algo
+
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int r = 0;
-        int w = 0;
-        int b = 0;
-        for (auto i : nums) {
-            if (i == 0) r++;
-            else if (i == 1) w++;
-            else b++;
-        }
-        int i = 0;
-        for (i; i < r; i++) {
-            nums[i] = 0;
-        }
-        for (i = r; i < r + w; i++) {
-            nums[i] = 1;
-        }
-        for (i = r + w; i < r + w + b; i++) {
-            nums[i] = 2;
+
+    void sortColors(vector<int>& v) {
+        int n = v.size();
+        int l = 0;
+        int m = 0;
+        int r = n - 1;
+        while (m <= r) {
+            // cout<<"l : "<<l<<" m : "<<m<<" r : "<<r<<"\n";
+            if (v[m] == 0) {
+                swap(v[m], v[l]);
+                m++;
+                l++;
+            }
+            else if (v[m] == 1) {
+                m++;
+            }
+            else {
+                swap(v[m], v[r]);
+                r--;
+            }
+            // for(auto i:v) {
+            //     cout<<i<<"  ";
+            // }
+            // cout<<"\n";
         }
     }
 };
